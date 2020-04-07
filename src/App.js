d93fd529eda836e918f724d1668f461caeeec838
P.C. Ndayisenga <paccy@Pacifiques-MacBook-Pro.local>
2020-04-07 12:32:50 +0200

    Configuring sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import './assets/scss/main.scss';
 import Home from './views/Home';
 import Login from './views/auth/Login';
 
 function App() {
   return (
     <Switch>
-      <Route exact path='/' component={Home} />
-      <Route path='/login' component={Login} />
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
     </Switch>
   );
 }
