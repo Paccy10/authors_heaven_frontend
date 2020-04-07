3735509f3794f7d602292eebbabf89cc4ddd38d0
P.C. Ndayisenga <paccy@Pacifiques-MacBook-Pro.local>
2020-04-07 12:00:58 +0200

    Setup react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './views/Home';
+import Login from './views/auth/Login';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/login' component={Login} />
+    </Switch>
   );
 }
 
